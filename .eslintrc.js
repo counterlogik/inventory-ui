bcd0fcfa2b3f15baccf787e5a61744bd83df57b9
addison staples <jastaplesiv@gmail.com>
2020-04-20 03:28:23 -0500

    linting, formatting were configured, applied

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['react-app', 'plugin:jsx-a11y/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'prettier', 'react', 'react-hooks'],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': 'error',
+    'import/no-deprecated': 'warn',
+    'import/order': 'warn',
+    'max-len': 'off',
+    'no-debugger': 'error',
+    'no-unused-expressions': 'error',
+    'no-unused-labels': 'error',
+    'prettier/prettier': 'error',
+    'quote-props': ['error', 'as-needed'],
+    semi: 'error',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+};
