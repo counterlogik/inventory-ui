dfc8f279d25b26153563e542282c114b276ea1df
addison staples <jastaplesiv@gmail.com>
2020-04-20 07:05:34 -0500

    integrate material ui etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <h3>inventory</h3>
+      <h6>Placeholder while testing Material UI + more for now.</h6>
+      <Button variant='contained' color='primary'>
+        nothing here
+      </Button>
+      <IconButton color='primary' aria-label='love me' component='span'>
+        <FavoriteBorderOutlinedIcon />
+      </IconButton>
     </div>
   );
 }
