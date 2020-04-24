3901407a9b93a988778851ac3a1e5cd64b3cd173
addison staples <jastaplesiv@gmail.com>
2020-04-24 11:29:58 -0500

    fix typo for get items call

--- src/pages/ItemCollection.tsx
+++ src/pages/ItemCollection.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { RouteComponentProps } from '@reach/router';
-import { GetItemDocument, GetItemsQuery, Item } from '../generated/graphql';
+import { GetItemsDocument, GetItemsQuery, Item } from '../generated/graphql';
 
 export default function ItemCollection(props: RouteComponentProps) {
-  const { loading, error, data } = useQuery<GetItemsQuery>(GetItemDocument);
+  const { loading, error, data } = useQuery<GetItemsQuery>(GetItemsDocument);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
